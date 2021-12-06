cc29df41c9ad6d70c4a174029d5b0e10d7454c02
vincent <adenijitosin112@gmail.com>
2021-12-06 04:22:30 -0500

    working on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import AboutScreen from "./about2.svg";
+import './about.css';
+import Chef from './chef.svg'
 
 
 export function About() {
     return (
         <div>
-            <h1> About </h1>
             <div style={{ height: "100%", display: "flex", justifyContent: "center", color: "white", flexDirection: "column", alignItems: "center"}}>
                 <img src={AboutScreen} alt="" width="300"/>
             </div>
+
+
+            <section className="aboutPage">
+                <section className="aboutContainer">
+                    <section className="aboutHeader">
+                        <h1> Chef and Designer Jeff </h1>
+                        <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                        </p>
+                    </section>
+
+                    <section className="contact">
+                        <h1> Contact me</h1>
+                        <section className="contactIcon">
+                            <p> facebook Icon here </p>
+                            <p> twitter Icon here </p>
+                            <p> linkedin Icon here </p>
+                            <p> instagram Icon here </p>
+                        </section> 
+                    </section>
+                </section>
+                
+
+                <section>             
+                    <img src={Chef} alt="chef" />
+                </section> 
+            </section>
         </div>
     )
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ContactScreen from "./contact.svg";
+import ContactImage from './Contact1.svg';
+import './Contact.css'
 
 
 export function Contact() {
@@ -8,7 +10,53 @@ export function Contact() {
             <div style={{ height: "100%", display: "flex", justifyContent: "center", color: "white", flexDirection: "column", alignItems: "center"}}>
                 <img src={ContactScreen} alt="" width="300"/>
             </div>
-            
+
+            <header> 
+                <h3 className="contactHead"> Contact me </h3>
+            </header>
+
+            <div className="contactContainer">
+
+                <div className="contact">
+                    <h1> Let's get in touch </h1>
+
+                    <form>
+
+                        <label> Full Name* </label>
+                        <input 
+
+                        />
+
+                        <label> Email Id* </label>
+                        <input
+
+                        />
+
+                        <label> Subject* </label> 
+                        <input
+                        />
+
+                        <label> Message* </label>
+                        <input
+                        placeholder="Your message here"
+                        className="msg"
+                        />
+
+                        <button> 
+                            <h4> Send </h4>
+                        </button>
+
+
+                    </form>
+
+                </div>
+
+                <div className="img">
+                    <img src={ContactImage} alt="#" width="100%" />
+                </div>
+
+                
+            </div>            
             
             
         </div>
