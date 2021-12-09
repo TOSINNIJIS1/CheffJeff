ce27e1a8e5ef977f644948d7dfb1f4de648c0a4b
vincent <adenijitosin112@gmail.com>
2021-12-08 19:35:50 -0500

    done with about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ import Chef from './chef.svg'
 
 export function About() {
     return (
-        <div>
+        <div className="aboutPageContainer">
             <div style={{ height: "100%", display: "flex", justifyContent: "center", color: "white", flexDirection: "column", alignItems: "center"}}>
                 <img src={AboutScreen} alt="" width="300"/>
             </div>
@@ -15,28 +15,26 @@ export function About() {
             <section className="aboutPage">
                 <section className="aboutContainer">
                     <section className="aboutHeader">
-                        <h1> Chef and Designer Jeff </h1>
+                        <h1> Legendary Jeff </h1>
                         <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
                         </p>
                     </section>
 
                     <section className="contact">
-                        <h1> Contact me</h1>
+                        {/* <h1 className="contactHead"> Contact me</h1> */}
                         <section className="contactIcon">
-                            <p> facebook Icon here </p>
-                            <p> twitter Icon here </p>
-                            <p> linkedin Icon here </p>
-                            <p> instagram Icon here </p>
+                            <a href="https://www.facebook.com/" target="_blank"  className="fa fa-facebook"></a>
+                            <a href="https://www.facebook.com/" target="_blank"  className="fa fa-twitter"></a>
+                            <a href="https://www.facebook.com/" target="_blank"  className="fa fa-linkedin"></a>
+                            <a href="https://www.facebook.com/" target="_blank"  className="fa fa-instagram"></a>
                         </section> 
                     </section>
                 </section>
                 
 
-                <section>             
-                    <img src={Chef} alt="chef" />
+                <section className="aboutImg">             
+                    <img src={Chef} alt="chef" width="100%" />
                 </section> 
             </section>
         </div>
