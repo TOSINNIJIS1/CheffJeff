04bd4bd2ea067da22d5fecb690efd2030e0f744d
vincent <adenijitosin112@gmail.com>
2021-12-08 20:39:28 -0500

    about page little fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import ContactScreen from "./contact.svg";
 import ContactImage from './Contact1.svg';
-import './Contact.css'
+import './Contact.css';
+import emailjs from 'emailjs-com'
 
 
 export function Contact() {
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm('Tosinnijis', 'porto_template', e.target, 'user_bHTPzVdnA6AwTs8F6mu2b')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text)
+        })
+        e.target.reset()
+    }
+    
     return (
         <div>
             <div style={{ height: "100%", display: "flex", justifyContent: "center", color: "white", flexDirection: "column", alignItems: "center"}}>
