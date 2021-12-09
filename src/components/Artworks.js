f8271b5a9d114f4ecba557f53602d69b556bfde4
vincent <adenijitosin112@gmail.com>
2021-12-08 21:37:23 -0500

    netlify issue

--- src/components/Artworks.js
+++ src/components/Artworks.js
@@ -7,8 +7,8 @@ const ArtWorks = () => {
             <h1 className="artHeader" style={{color: "white", marginTop: "1em"}}> Artworks </h1>
 
             {/* <div className="wrapper" > */}
-                <div class="row"> 
-                    <div class="column">
+                <div className="row"> 
+                    <div className="column">
                         <img src="https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" width="100%" alt="" />
                         <img src="https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=445&q=80" width="100%" alt="" />
                         <img src="https://images.unsplash.com/photo-1551732998-9573f695fdbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" width="100%" alt="" />
@@ -18,7 +18,7 @@ const ArtWorks = () => {
                         <img src="https://www.pikpng.com/pngl/b/34-340038_venom-image-venom-imax-poster-clipart.png" width="100%" alt="" />
                     </div>
 
-                    <div class="column">
+                    <div className="column">
                         <img src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1245&q=80" width="100%" alt="" />
                         <img src="https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" width="100%" alt="" />
                         <img src="https://images.unsplash.com/photo-1537632083056-f557a4e1e01f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" width="100%" alt="" />
@@ -26,7 +26,7 @@ const ArtWorks = () => {
                         <img src="https://images.unsplash.com/photo-1493210977798-4f655ac200a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=952&q=80" width="100%" alt="" />
                         <img src="https://images.unsplash.com/photo-1603344204980-4edb0ea63148?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" width="100%" alt="" />
                     </div>  
-                    <div class="column">
+                    <div className="column">
                         <img src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" width="100%" alt="" />
                         <img src="https://images.unsplash.com/photo-1549887552-cb1071d3e5ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80" width="100%" alt="" />
                         <img src="https://images.unsplash.com/photo-1571115764595-644a1f56a55c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=441&q=80" width="100%" alt="" />
