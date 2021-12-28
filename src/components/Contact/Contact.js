1b620ff09adf23bbc6f7e835c5ecf5612b06fb63
vincent <adenijitosin112@gmail.com>
2021-12-27 20:55:31 -0500

    contact changed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,22 +2,22 @@ import React from 'react'
 import ContactScreen from "../Contact/contact.svg";
 import ContactImage from '../Contact/Contact1.svg';
 import '../Contact/Contact.css';
-// import emailjs from 'emailjs-com'
+import emailjs from 'emailjs-com'
 
 
 export function Contact() {
-    // const onSubmit = (e) => {
-    //     e.preventDefault()
+    const onSubmit = (e) => {
+        e.preventDefault()
 
-    //     emailjs.sendForm('Tosinnijis', 'porto_template', e.target, 'user_bHTPzVdnA6AwTs8F6mu2b')
-    //     .then((result) => {
-    //         console.log(result.text);
-    //     }, (error) => {
-    //         console.log(error.text)
-    //     })
-    //     e.target.reset()
-    // }
-    
+        emailjs.sendForm('Tosinnijis', 'porto_template', e.target, 'user_bHTPzVdnA6AwTs8F6mu2b')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text)
+        })
+        e.target.reset()
+    }
+
     return (
         <div>
             <div style={{ height: "100%", display: "flex", justifyContent: "center", color: "white", flexDirection: "column", alignItems: "center"}}>
@@ -33,29 +33,40 @@ export function Contact() {
                 <div className="contact">
                     <h1> Let's get in touch </h1>
 
-                    <form>
+                    <form onSubmit={onSubmit} style={{marginBottom: '16%'}}>
 
                         <label> Full Name* </label>
                         <input 
+                        type='name'
+                        name='name'
+                        required
 
                         />
 
                         <label> Email Id* </label>
                         <input
-
+                        type="email"
+                        name="email"
+                        required
                         />
 
                         <label> Subject* </label> 
                         <input
+                        type='text'
+                        name='subject'
+                        required
                         />
 
                         <label> Message* </label>
-                        <input
+                        <textarea
                         placeholder="Your message here"
                         className="msg"
+                        type='text'
+                        name='message'
+                        required
                         />
 
-                        <button disabled> 
+                        <button className='button'> 
                             <h4> Send </h4>
                         </button>
 
