cf0d81651f2cfec268aeddc57a0025fb1a555f41
vincent <adenijitosin112@gmail.com>
2022-01-28 00:13:02 -0500

    fixing the text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     const content = useContext(MyContextApi)    
     const footer = content.map(data => data.footer)
     return (
-        <div style={{background:"#313843", color: "white", padding: '1.23rem', fontFamily: 'Freight', fontWeight: '700', flex: 1, position: 'fixed', left: '0', bottom: 0, width: '100%', textAlign: 'center', borderTop: 'rgb(128, 116, 116) solid 1px'}}>
+        <div className='footer'>
             <div style={{display: 'flex', justifyContent: 'center'}}>
                 <footer> {footer} </footer>
             </div>
