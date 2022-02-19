e0553c277c86e394673195e8f08c812791ec2a6a
vincent <adenijitosin112@gmail.com>
2022-02-19 15:22:07 -0500

    fixed the content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,47 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { MyContextApi } from '../../useContext/context';
 import ArtWorks from '../Arts/Artworks';
 import Modal from '../Arts/Modal';
 import Carousel from '../Carousel/JeffCarousel';
-import HomeScreen from "../Home/home.svg";
-
-// import { MyContextApi } from '../../useContext/context';
 import useFirestore from '../Firestore/useFirestore';
-import { data } from 'flickity';
 
 
 
 export function Home({ selectedImg, setSelectedImg, selectedTitle, setSelectedTitle }) {
+    // Custom hook for firestore
+    const value = useContext(MyContextApi)
     const {docs} = useFirestore('artworks')
-
-    const info = Object.keys(docs).map((id) => docs[id].docs)
-    const urlImg = docs.map((data, i) =>   data.select === 'Logo' && i === 0  ? data.url : 'https://firebasestorage.googleapis.com/v0/b/cheffjeffart.appspot.com/o/artworks%2FFB_IMG_1634775512484.jpg?alt=media&token=5bd56425-00f4-4137-8315-88b0160cef6b' 
-    )
-
+    let content = value.map(data => data.content)
+    // Map the object for the documents
+    // const info = Object.keys(docs).map((id) => docs[id])
+    // console.log(info)
+    // map and filtered the logo using the ternary operator
+    const urlImg = docs.map((data, i) => data.select === 'Logo' && i === 0  ? data.url : 'https://firebasestorage.googleapis.com/v0/b/cheffjeffart.appspot.com/o/artworks%2FFB_IMG_1634775512484.jpg?alt=media&token=5bd56425-00f4-4137-8315-88b0160cef6b')
 
     return (
         <div>
             <div style={{ height: "100%", display: "flex", justifyContent: "center", color: "white", flexDirection: "column", alignItems: "center"}}>
-                {
-                // docs.filter((data) => {
-                //     // console.log(data)
-                //     // if (data.select === 'Logo') {
-                //     //     // console.log(data)
-                //     //     return data.select
-                //     // }
-                // })
-                docs.map((data, i) =>  {
-                    // <img src={HomeScreen} width="300"/>
-
-                    // if (data.select === null || data.select === '' || data.select === undefined) {
-                    //     return <img src={HomeScreen} width="300"/>
-                    // } else { return }
-                    // data.select === 'Logo' && i === 0  ? <img src={data.url || HomeScreen} alt="" width="300"/>: null 
-                }
-                 )}
                 <img src={urlImg} width="300" />
 
 
-                <p style={{fontFamily: "Freight", fontStyle: "italic", fontWeight: "400", fontSize: 21}}> {info} </p>
+                <p style={{fontFamily: "Freight", fontStyle: "italic", fontWeight: "400", fontSize: 21}}> {content} </p>
             </div>
             
             <div><Carousel /></div>
